9c2e017be7b8bae4f31b7bab289d9e95a7550efe
Cheng Lu <jason.cheng.lu@hotmail.com>
2022-05-29 15:03:53 +1200

    Add unit test to verify the detection impl

--- TestMinesweeperAnalysis/TestMinesweeperAnalysis.cpp
+++ TestMinesweeperAnalysis/TestMinesweeperAnalysis.cpp
@@ -10,14 +10,20 @@ namespace TestMinesweeperAnalysis
 	{
 	public:
 		
-		TEST_METHOD(TestValidConfiguration)
+		TEST_METHOD(TestValidBoardConfiguration)
 		{
-			Assert::AreEqual(true, true);
+			tuple <bool, bool> ret;
+			/* A 4 x 2 board with 4 mines */
+			ret = SolutionValidation(4, 2, 4);
+			Assert::AreEqual(true, get<0>(ret));
 		}
 
-		TEST_METHOD(TestInValidConfiguration)
+		TEST_METHOD(TestInValidBoardConfiguration)
 		{
-			Assert::AreEqual(false, false);
+			tuple <bool, bool> ret;
+			/* A 4 x 4 board with 14 mines */
+			ret = SolutionValidation(4, 4, 14);
+			Assert::AreEqual(false, get<0>(ret));
 		}
 	};
 }
