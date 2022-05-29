3059b9b17adcd5e503ad45e54f4fc028b6e7941a
Cheng Lu <jason.cheng.lu@hotmail.com>
2022-05-29 14:28:41 +1200

    Setup the main function with a loop

--- MinesweeperAnalysis.cpp
+++ MinesweeperAnalysis.cpp
@@ -40,5 +40,42 @@ tuple <int, int, int> GetUserInputs(void)
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string ans("");
+    while (ans != "exit") {
+        cout << "Hello ! Welcome to Cheng's Minesweeper Analyzer " << endl;
+        cout << "A and B represents the size of the board" << endl;
+        cout << "N means the number of mines" << endl;
+
+        bool isInputsValid = false;
+        tuple <int, int, int> inputs;
+
+        while (!isInputsValid)
+        {
+            inputs = GetUserInputs();
+            if ((get<0>(inputs) < 2 && get<1>(inputs) < 2) || get<2>(inputs) == 0 || get<2>(inputs) >= (get<0>(inputs) * get<1>(inputs)))
+            {
+                cout << "Wrong configuration detected ! Please check and reconfigure" << endl;
+                cout << "The number of mines need to be less than the neumber of squares and must not be 0" << endl;
+                cout << "The size of the game board must have at least two squares" << endl;
+            }
+            else
+            {
+                isInputsValid = true;
+            }
+        }
+
+       
+
+        cout << endl;
+        cout << endl;
+        cout << "Try again ? " << endl;
+        cout << "Input exit to quit this program " << endl;
+        cout << "Input any other key to run again " << endl;
+        cout << "Please type: ";
+        cin >> ans;
+        cout << endl;
+        cout << endl;
+    }
+
+    return 0;
 }
